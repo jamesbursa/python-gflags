00f5c9d8cc3fa45e5be4690ca5c10c1a32d965bb
Joachim Metz <joachim.metz@gmail.com>
2016-01-17 09:42:40 +0100

    Changes to setup.py for bdist_rpm.

--- setup.py
+++ setup.py
@@ -29,7 +29,90 @@
 
 """Setup module for python-gflags."""
 
-from setuptools import setup
+import sys
+
+try:
+  from setuptools import find_packages, setup, Command
+except ImportError:
+  from distutils.core import find_packages, setup, Command
+
+try:
+  from setuptools.commands.bdist_rpm import bdist_rpm
+except ImportError:
+  from distutils.command.bdist_rpm import bdist_rpm
+
+
+class BdistRPMCommand(bdist_rpm):
+  """Custom handler for the bdist_rpm command."""
+
+  def _make_spec_file(self):
+    """Generates the text of an RPM spec file.
+
+    Returns:
+      A list of strings containing the lines of text.
+    """
+    # Note that bdist_rpm can be an old style class.
+    if issubclass(BdistRPMCommand, object):
+      spec_file = super(BdistRPMCommand, self)._make_spec_file()
+    else:
+      spec_file = bdist_rpm._make_spec_file(self)
+
+    if sys.version_info[0] < 3:
+      python_package = 'python'
+    else:
+      python_package = 'python3'
+
+    description = []
+    name = ''
+    summary = ''
+    in_description = False
+
+    python_spec_file = []
+    for index, line in enumerate(spec_file):
+      if line.startswith('%define name '):
+        name = line[20:]
+
+      elif line.startswith('Summary: '):
+        summary = line
+
+      elif line.startswith('BuildRequires: '):
+        line = 'BuildRequires: {0:s}-setuptools'.format(python_package)
+
+      elif line.startswith('Requires: '):
+        if python_package == 'python3':
+          line = line.replace('python', 'python3')
+
+      elif line.startswith('%description'):
+        in_description = True
+
+      elif line.startswith('%files'):
+        if python_package == 'python3':
+          line = '%files -f INSTALLED_FILES -n {0:s}-{1:s}'.format(
+             python_package, name)
+
+      elif line.startswith('%prep'):
+        in_description = False
+
+        if python_package == 'python3':
+          python_spec_file.append(
+              '%package -n {0:s}-{1:s}'.format(python_package, name))
+          python_spec_file.append('{0:s}'.format(summary))
+          python_spec_file.append('')
+          python_spec_file.append(
+              '%description -n {0:s}-{1:s}'.format(python_package, name))
+          python_spec_file.extend(description)
+
+      elif in_description:
+        # Ignore leading white lines in the description.
+        if not description and not line:
+          continue
+
+        description.append(line)
+
+      python_spec_file.append(line)
+
+    return python_spec_file
+
 
 setup(name='python-gflags',
       version='3.0.0',
@@ -38,7 +121,9 @@ setup(name='python-gflags',
       author='Google Inc. and others',
       author_email='google-gflags@googlegroups.com',
       url='https://github.com/google/python-gflags',
+      packages=find_packages('.'),
       py_modules=['gflags'],
       data_files=[('bin', ['gflags2man.py'])],
       include_package_data=True,
+      cmdclass={'bdist_rpm': BdistRPMCommand},
      )
